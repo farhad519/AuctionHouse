8573db4a0faf9a436f745ae1d9690e1648bec4cd
farhad519 <farhadchowdhury519@gmail.com>
2021-12-03 17:53:22 +0600

    Implement sign up ui

--- AuctionHouse/SceneDelegate.swift
+++ AuctionHouse/SceneDelegate.swift
@@ -17,8 +17,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let windowScene = (scene as? UIWindowScene) else { return }
+        let storyboard = UIStoryboard(name: "SignUp", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "SignUpMainViewController")
         let window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: DashboardViewController())
+        let navigationController = UINavigationController(rootViewController: vc)
         window.rootViewController = navigationController
         self.window = window
         window.makeKeyAndVisible()
